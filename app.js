8be8600d511838f81c7bff15851ea01cfd1b5d4f
Najma Saeed <najmasy20@gmail.com>
2023-09-21 20:42:47 +0100

    fix functions

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 let userPoints = 0;
-let user = prompt("What is your name?");
 
-alert("Hello " + user + ". Welcome to my website about me");
+function getUserInput(promptMessage) {
+  let userInput = prompt("What is your name?");
+  alert("Hello " + userInput + ". Welcome to my website about me");
+}
 
-let eduQ = prompt(
-  "Do i have a degree in Psychology? (yes or no)"
-).toLowerCase();
-// to lower - changes all answers to lowercase so it matches if statement
+function askQuestion(promptMessage, correctAnswer) {
+  let userAnswer = prompt(promptMessage);
+  while (userAnswer !== correctAnswer) {
+    userAnswer = prompt("Wrong answer. " + promptMessage);
+  }
+  alert("Correct, " + correctAnswer);
+}
 
+//when button is clicked
+
+function startQuiz() {
+  let userInput = prompt("What is your name?");
+  alert("Hello " + userInput + ". Welcome to my website about me");
+
+  let eduQuestion = "Do i have a degree in Psychology? (yes or no)";
+  let eduQ = getUserInput(eduQuestion);
+
+  if (eduQ === "yes") {
+    alert("YES!I do have a psychology degree");
+  } else {
+    alert("Wrong Answer!");
+  }
+
+  /*
 while (eduQ !== "yes" && eduQ !== "no") {
   eduQ = prompt(
     "Got to be yes or no. Do i have a degree in Psychology?"
@@ -22,16 +43,30 @@ if (eduQ === "yes") {
 
 console.log(eduQ);
 
-let age = prompt("How old am I?");
-//not infinite loop - used ans = prompt
+*/
+
+  let ageQuestion = "How old am I?";
+  askQuestion(askQuestion, "21");
+
+  /*
 while (age !== "21") {
   age = prompt("wrong answer");
 }
 alert("Correct, i am 21 years old");
+*/
 
-let homeQ = prompt("Do i live in Liverpool (yes or no)").toLowerCase();
+  let homeQuestion = "Do i live in Liverpool (yes or no)";
+  let homeQ = getUserInput(homeQuestion);
 
-while (homeQ !== "yes" && eduQ !== "no") {
+  if (homeQ === "yes") {
+    alert("YES!I do live in Liverpool, home of the beatles");
+  } else {
+    alert("Wrong Answer!");
+  }
+
+  /*
+
+while (homeQ !== "yes" && homeQ !== "no") {
   homeQ = prompt("Try again. Do i live in Liverpool?").toLowerCase();
 }
 
@@ -43,13 +78,54 @@ if (homeQ === "yes") {
 
 console.log(homeQ);
 
-let moveHouse = prompt("Where do i want to live in the future?");
+*/
 
-while (moveHouse !== "London") {
-  moveHouse = prompt("try again");.toLowerCase
+  let moveHouseQuestion = "Where do i want to live in the future?";
+  let moveHouse = getUserInput(moveHouseQuestion);
+
+  if (moveHouse === "london") {
+    alert("YES!I want to move to London in the future");
+  } else {
+    alert("Wrong Answer!");
+  }
 }
-if (moveHouse === "London") {
-  alert("YES!I want to move to London in the future");
-} else {
-  alert("Wrong Answer!");
+/*
+while (moveQ !== "London") {
+  moveHouse = prompt("try again").toLowerCase();
 }
+*/
+
+function startNumberQuiz() {
+  let answer = 5;
+  let number = prompt("Guess a number between 1 and 10");
+
+  for (let i = 1; i <= 4; i++) {
+    if (number > answer) {
+      alert("too high");
+    } else if (number < answer) {
+      alert("too low");
+      number = prompt("Guess again");
+    } else if (parseInt(number) === answer) {
+      alert("That right! The answer was 5");
+      break;
+    }
+  }
+
+  let list = ["Dubai", "France", "Italy", "America"];
+  let guess = prompt("What country do i want to visit in the world");
+
+  let idx = list.indexOf(guess);
+  console.log(idx);
+}
+//Q6 - takes in numeric input
+//prompt to guess a number
+//alert - too high/low
+//four attempts
+//after attempt - give correct answer - loop
+
+//Q7-multple possible correct answers
+//give user 6 attempts
+
+//give final score out of 7
+//use console.log throughout to track questions
+//add css
